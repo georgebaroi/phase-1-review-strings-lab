6652a2e43858e22380bcc53e0f644ce97a31b575
George Baroi <baroi.george@gmail.com>
2022-07-18 16:15:02 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser= "George"
+let welcomeMessage= "Welcome to Flatbook, George!"
+let excitedWelcomeMessage= "WELCOME TO FLATBOOK, GEORGE!"
+let shortGreeting = "Welcome, G!"
